4a501ce0e14b5cf40b382d1f4cbd07c1e70bdd06
DESKTOP-4FEPUV7\36978 <3697897286@qq.com>
2025-07-23 16:20:21 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestDemo
+{
+    class Pro
+    {
+        public static void Main(String[] args)
+        {
+            #region
+            Console.WriteLine("梦想还是要有的，万一实现了呢");
+            #endregion
+
+        }
+    }
+}
