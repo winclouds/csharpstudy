de10af0cf6beb91b91fc0f16e6c8362d4cce2792
DESKTOP-4FEPUV7\36978 <3697897286@qq.com>
2025-07-24 10:49:21 +0800

    新学输入和任意键

--- Program.cs
+++ Program.cs
@@ -7,7 +7,24 @@ namespace TestDemo
         public static void Main(String[] args)
         {
             #region
-            Console.WriteLine("梦想还是要有的，万一实现了呢");
+            Console.WriteLine("请输入您的姓名");
+            String? nameInput = Console.ReadLine(); // 允许 null
+            String name = nameInput ?? ""; // 如果为 null，赋值为空字符串
+            Console.ReadKey();
+            Console.WriteLine(name);
+            Console.WriteLine("请输入您的年龄");
+            string? ageInput = Console.ReadLine();
+            int age = Convert.ToInt32(ageInput ?? "0"); // 如果为 null，赋值为 "0"
+            Console.ReadKey();
+            Console.WriteLine(age);
+            Console.WriteLine("请输入您的班级：");
+            String? classNameInput = Console.ReadLine();
+            String className = classNameInput ?? "";
+            Console.ReadKey();
+            Console.WriteLine(className);
+
+            Console.Write("好好学习，天天向上"); // 输出到控制台且不换行
+            Console.Write("努力工作，快乐生活");
             #endregion
 
         }
